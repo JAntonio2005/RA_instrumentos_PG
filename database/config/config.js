c6bf10b3e834f22ef28e85945a17ab881e2f6604
José Ortiz Hernández <joseantonioortizhernandez777@gmail.com>
2025-04-23 02:47:23 -0600

    ASDASDSD

--- database/config/config.js
+++ database/config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   development: {
     url: process.env.DEV_DATABASE_URL,
-    dialect: 'mysql',
+    dialect: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
@@ -13,7 +13,7 @@ module.exports = {
   },
   test: {
     url: process.env.TEST_DATABASE_URL,
-    dialect: 'mysql',
+    dialect: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
@@ -23,7 +23,7 @@ module.exports = {
   },
   production: {
     url: process.env.DATABASE_URL,
-    dialect: 'mysql',
+    dialect: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
